a59d984c551c062f5da6f51766dd199932bb4136
jefftlin <jefft_lin@163.com>
2025-04-09 16:25:04 +0800

    Minor fix

--- exchangis-datasource/exchangis-datasource-service/src/main/java/com/webank/wedatasphere/exchangis/datasource/service/impl/DefaultDataSourceService.java
+++ exchangis-datasource/exchangis-datasource-service/src/main/java/com/webank/wedatasphere/exchangis/datasource/service/impl/DefaultDataSourceService.java
@@ -858,14 +858,7 @@ public class DefaultDataSourceService extends AbstractDataSourceService
                 LinkisDataSourceRemoteClient::getConnectParams, CLIENT_DATASOURCE_PARAMS_GET_ERROR.getCode(),
                 "");
 
-        Map<String, Object> connectParams = result.getConnectParams();
-        if (Objects.nonNull(connectParams) && connectParams.containsKey("password")
-                && StringUtils.equals(String.valueOf(connectParams.get("isEncrypt")), "1")) {
-            String decrypt = AESUtils.decrypt(connectParams.get("password").toString(),
-                    LINKIS_DATASOURCE_AES_KEY.getValue());
-            connectParams.replace("password", decrypt);
-        }
-        return connectParams;
+        return result.getConnectParams();
     }
 
     /**
